b73b1bdb340be962202678b4e3e796d8167705b1
theJOHNwatt <j.watt10@gmail.com>
2019-09-20 13:59:23 -0700

    :rocket: MVP
    completed mvp

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, {Component} from "react";
+import styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const CardContainer = styled.div `
+display: inline-flex;
+margin: 15px;
+text-align: center;
+background: rgba(187, 159, 0, 0.699);
+border-radius: 5px;
+box-shadow: 0px 0px 10px 5px rgba(0,0,0,.1);
+`;
+
+const Card = styled.div `
+
+
+`;
+const CardDiv = styled.div`
+
+
+`;
+
+
+export default function CharacterCard(props) {
+  return (
+    
+   <CardContainer>
+    <Card
+       key={props.id}>
+         <CardDiv>
+        <h3>{props.name}</h3>
+       <img src={props.image}></img>
+        <p>Species: {props.species}</p> 
+        <p>Gender: {props.gender}</p>
+        </CardDiv>
+    </Card>
+    </CardContainer> 
+  )
 }
+
+
+
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../index.css";
+import styled from 'styled-components';
+
+const NavBar = styled.div `
+// border: 1px red solid;
+display: flex;
+justify-content: space-evenly;
+width:80%;
+margin: 0 auto;
+border-radius: 5px;
+h2{
+  color: black;
+}
+box-shadow: 0px 0px 10px 5px rgba(0,0,0,.1);
+`;
+
+const Heading = styled.div `
+h1{
+// border: 1px red solid;
+text-align: center;
+
+}
+`;
 
 export default function Header() {
   return (
-    <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
-    </header>
+    <Heading>
+      <h1>Rick and Morty Fan Page</h1>
+      <NavBar className='nav'>
+        <NavLink key={1} to={'/'}><h2>Home</h2></NavLink>
+        <NavLink key={2} to={'/list/'}><h2>Characters</h2></NavLink> 
+        <NavLink key={3} to={'/search/'}><h2>Search</h2></NavLink>      
+      </NavBar>
+    </Heading>
   );
 }
